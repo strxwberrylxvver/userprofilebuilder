9078b89f3f4caf4460b58256ab41b1ab8d916384
k2372003 <k2372003@PPRLRC03-300218.kuds.kingston.ac.uk>
2025-01-14 17:22:42 +0000

    W1R2 – Add Layout and Name 'Edit' Panel

--- src/main/java/userprofilebuilder/view/MainViewer.java
+++ src/main/java/userprofilebuilder/view/MainViewer.java
@@ -7,18 +7,30 @@ package userprofilebuilder.view;
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.ScrollPane;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
 
 
 public class MainViewer {
     private JFrame myframe;
+    private JTextArea yap;
+    private JTabbedPane tabs = new JTabbedPane();
+
     public MainViewer(){
     myframe = new JFrame("user profile builder <3");
     myframe.setBounds(400,0,400,400);
     myframe.setLayout(new BorderLayout());
     JPanel mypanel = new JPanel();
+    mypanel.setBackground(Color.PINK);
     mypanel.setLayout(new FlowLayout());
+    //this.yap = new JTextArea();
+    //mypanel.add(yap, "hi");
+    tabs.addTab("WELCOME", mypanel);
+    myframe.add(tabs);
     myframe.add(mypanel,BorderLayout.CENTER);
     myframe.setVisible(true);
     myframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
