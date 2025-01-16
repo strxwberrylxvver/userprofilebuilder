f454dc6697dbaf177e3de19bb15392feec8fdb36
k2372003 <k2372003@PPRLRC03-300218.kuds.kingston.ac.uk>
2025-01-16 17:20:54 +0000

    W1R3 – Add specified components to the Edit Panel

--- src/main/java/userprofilebuilder/view/MainViewer.java
+++ src/main/java/userprofilebuilder/view/MainViewer.java
@@ -7,6 +7,7 @@ package userprofilebuilder.view;
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridLayout;
 import java.awt.ScrollPane;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
@@ -15,7 +16,8 @@ import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
-
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
 
 public class MainViewer {
     private JFrame myframe;
@@ -23,13 +25,23 @@ public class MainViewer {
     
     public MainViewer(){
     myframe = new JFrame("user profile builder <3");
-    myframe.setBounds(400,0,400,400);
+    myframe.setSize(400,400);
     myframe.setLayout(new BorderLayout());
     
     JPanel mypanel = new JPanel();
     mypanel.setBackground(Color.PINK);
-    mypanel.setLayout(new FlowLayout());
+    mypanel.setLayout(new GridLayout(1,2));
     mypanel.setBorder(BorderFactory.createTitledBorder("Name"));
+    
+    JButton edt = new JButton("Edit");
+    JButton dlt = new JButton("Delete");
+    JRadioButton bu = new JRadioButton();
+    JLabel un = new JLabel("usernames");
+    
+    mypanel.add(bu,BorderLayout.EAST);
+    mypanel.add(un,BorderLayout.CENTER);
+    mypanel.add(edt,BorderLayout.CENTER);
+    mypanel.add(dlt,BorderLayout.WEST);
     
     myframe.add(mypanel,BorderLayout.CENTER);
     myframe.setVisible(true);
