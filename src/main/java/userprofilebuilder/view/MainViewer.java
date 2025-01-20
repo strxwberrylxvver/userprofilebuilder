66ef444d164e562a7a54e46d12d5dd876f52cf9b
k2372003 <k2372003@PPRLRC03-300206.kuds.kingston.ac.uk>
2025-01-20 20:07:41 +0000

    W1R6 – Adding Additional Tabbed Panes for data in the 'userprofile.csv' file, I made the panels for the emails,names and titles, along with edit & delete components.

--- src/main/java/userprofilebuilder/view/MainViewer.java
+++ src/main/java/userprofilebuilder/view/MainViewer.java
@@ -15,8 +15,6 @@ import javax.swing.JButton;
 import userprofilebuilder.model.User;
 import userprofilebuilder.model.UserGroup;
 
-
-
 public class MainViewer {
     private JFrame myframe;
     private JTextArea yap;
@@ -27,15 +25,19 @@ public class MainViewer {
     myframe.setLayout(new BorderLayout());
 
     JTabbedPane tabs = new JTabbedPane();
-    
+    JPanel ut = new JPanel();
+    JPanel ue = new JPanel();
+    ut.setLayout(new GridLayout(3,1));
+    ut.setBorder(BorderFactory.createTitledBorder("Title"));
+    ue.setLayout(new GridLayout(3,1));
+    ue.setBorder(BorderFactory.createTitledBorder("Email"));
+
     JPanel mypanel = new JPanel();
- //   mypanel.setBackground(Color.PINK);
     mypanel.setLayout(new GridLayout(3,1));
     mypanel.setBorder(BorderFactory.createTitledBorder("Name"));
     
     for (User u: UserGroup.getUsers()) {
         JPanel rows = new JPanel();
-       // rows.setBackground(Color.PINK);
         JRadioButton j = new JRadioButton(u.getName());
         j.setPreferredSize(new Dimension(130,30));
         rows.add(j);
@@ -48,6 +50,38 @@ public class MainViewer {
         rows.repaint();
         mypanel.add(rows,BorderLayout.CENTER);
     }
+    
+    for (User u: UserGroup.getUsers()) {
+        JPanel rows0 = new JPanel();
+        JRadioButton j = new JRadioButton(u.getTitle());
+        j.setPreferredSize(new Dimension(60,30));
+        rows0.add(j);
+        JButton e = new JButton("Edit");
+        e.setPreferredSize(new Dimension(65,25));
+        rows0.add(e);
+        JButton d = new JButton("Delete");
+        d.setPreferredSize(new Dimension(70,25));
+        rows0.add(d);
+        rows0.repaint();
+        ut.add(rows0,BorderLayout.CENTER);
+    }
+ 
+    for (User u: UserGroup.getUsers()) {
+        JPanel rows1 = new JPanel();
+        JRadioButton j = new JRadioButton(u.getEMail());
+        j.setPreferredSize(new Dimension(200,30));
+        rows1.add(j);
+        JButton e = new JButton("Edit");
+        e.setPreferredSize(new Dimension(65,25));
+        rows1.add(e);
+        JButton d = new JButton("Delete");
+        d.setPreferredSize(new Dimension(70,25));
+        rows1.add(d);
+        rows1.repaint();
+        ue.add(rows1,BorderLayout.CENTER);
+    }
+    tabs.addTab("User Title", ut);
+    tabs.addTab("User Email", ue);
     tabs.add(mypanel,"User Name");
     myframe.add(tabs,BorderLayout.CENTER);
     myframe.setVisible(true);
