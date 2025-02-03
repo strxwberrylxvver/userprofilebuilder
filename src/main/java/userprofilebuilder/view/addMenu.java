9df8f9bb116ad932a5ffb67056bf0d084eb04563
k2372003 <k2372003@PPRSB2024-F3.kuds.kingston.ac.uk>
2025-02-03 19:12:44 +0000

    W3R1 – Add a MenuBar -added functionality (allowed application to close)

--- src/main/java/userprofilebuilder/view/addMenu.java
+++ src/main/java/userprofilebuilder/view/addMenu.java
@@ -5,22 +5,43 @@
 package userprofilebuilder.view;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-public class addMenu extends JMenuBar{
+public class addMenu extends JMenuBar implements ActionListener{
     JMenu fileMenu;
     JMenuItem openFile; 
     JMenuItem quit;
-    public addMenu(JFrame f){   
+    public addMenu(JFrame frame){   
         fileMenu = new JMenu("File");
-        openFile = new JMenuItem("Open File...");
-        quit = new JMenuItem("Quit File...");
+        openFile = new JMenuItem("Open File");
+        quit = new JMenuItem("Quit");
+        
+        openFile.addActionListener(this);
+        openFile.setActionCommand("OpenFile");
+        quit.addActionListener(this);
+        quit.setActionCommand("QuitFile");
+
         fileMenu.add(openFile);
         fileMenu.add(quit);
         this.add(fileMenu);
-        f.add(this,BorderLayout.NORTH);
+        frame.add(this,BorderLayout.NORTH);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        switch(e.getActionCommand()){
+            case "OpenFile":
+                System.out.println("Open File");
+                break;
+            case "QuitFile":
+                System.out.println("Quit");
+                System.exit(0);
+                break;
+        }
     }
 }
