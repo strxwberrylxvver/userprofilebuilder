f893b0325fbba6558ec4630148054047027327c2
Merrone <merrone12345@gmail.com>
2025-02-11 02:09:26 +0000

    W3R3 – Add a Save File Option - used my writecsv method

--- src/main/java/userprofilebuilder/view/addMenu.java
+++ src/main/java/userprofilebuilder/view/addMenu.java
@@ -20,17 +20,22 @@ public class addMenu extends JMenuBar implements ActionListener{
     JMenu fileMenu;
     JMenuItem openFile; 
     JMenuItem quit;
+    JMenuItem saveFile;
     public addMenu(JFrame frame){   
         fileMenu = new JMenu("File");
         openFile = new JMenuItem("Open File");
         quit = new JMenuItem("Quit");
+        saveFile = new JMenuItem("Save File");
         
         openFile.addActionListener(this);
         openFile.setActionCommand("OpenFile");
         quit.addActionListener(this);
         quit.setActionCommand("QuitFile");
+        saveFile.addActionListener(this);
+        saveFile.setActionCommand("saveFile");
 
         fileMenu.add(openFile);
+        fileMenu.add(saveFile);
         fileMenu.add(quit);
         this.add(fileMenu);
         frame.add(this,BorderLayout.NORTH);
@@ -39,6 +44,7 @@ public class addMenu extends JMenuBar implements ActionListener{
     public void actionPerformed(ActionEvent e)
     {
         switch(e.getActionCommand()){
+
             case "OpenFile":
                 System.out.println("Open File");
                 JFileChooser chooser = new JFileChooser();
@@ -56,10 +62,31 @@ public class addMenu extends JMenuBar implements ActionListener{
                     }
                 }
                 break;
+
+            case "saveFile":
+                System.out.println("Save File");
+                JFileChooser choose = new JFileChooser();
+                FileNameExtensionFilter filte = new FileNameExtensionFilter(
+                        "CSV Files", "csv");
+                choose.setFileFilter(filte);
+                int returnval = choose.showSaveDialog(null);
+                if(returnval == JFileChooser.APPROVE_OPTION) {
+                    System.out.println("You chose to save this file: " +
+                            choose.getSelectedFile().getName());
+                    /*for (User u : UserGroup.getInstance().getUsers())
+                    {
+                        System.out.println(u);
+                    }*/
+                    UserGroup.getInstance().writeSuperCsv(choose.getSelectedFile().getName());
+                }
+                break;
+
             case "QuitFile":
                 System.out.println("Quit");
                 System.exit(0);
                 break;
-        }
+
+
+                }
     }
 }
